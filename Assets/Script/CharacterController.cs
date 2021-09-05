e97d47a5fd34dbca702ec02a5967a565441f1a58
shirota0413 <e185712@ie.u-ryukyu.ac.jp>
2021-09-05 15:04:49 +0900

    Move motion added

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    [SerializeField]private int characterHP;
+    [SerializeField]private float characterSpeed;
+    [SerializeField]private int characterPower;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    public int CharacterAnimation() {
+        return 0;
+    }
+
+    public int CharacterHPCaluculation(int Damerg) {
+        return 0;
+    }
+
+    public int CharacterDie() {
+        return 0;
+    }
+
+    public int CharacterAttack() {
+        return 0;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FiveTreasures
+![Test Image 1](ULC2.0/License Logo/Others/jpg/Light_Silhouette.jpg)
